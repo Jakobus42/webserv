682ae7eeb3626065262b6f6e94d80dbbf173823f
Jakobus42 <Jakob.sadjina@icloud.com>
2024-10-09 14:14:21 +0000

    chore: docs prefix as exception in branchName pre commit

--- .github/hooks/branchName.sh
+++ .github/hooks/branchName.sh
@@ -4,6 +4,10 @@ prefixes=("feat" "bugfix" "hotfix" "chore" "sandbox")
 
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 
+if [[ "$branch_name" == "docs" ]]; then
+  exit 0
+fi
+
 valid_prefix=false
 
 for prefix in "${prefixes[@]}"; do
@@ -15,7 +19,7 @@ done
 
 if [ "$valid_prefix" == false ]; then
   echo "Invalid branch name."
-  echo "Branch name should start with one of the following prefixes: ${prefixes[*]}"
+  echo "Branch name should start with one of the following prefixes: ${prefixes[*]}, or be named 'docs'."
   echo "Example: 'feat/hooks'"
   exit 1
 fi
