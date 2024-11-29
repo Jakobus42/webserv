c0b84ad4bdb843bd1928b0da0a283f57f0cbfe73
AJMCoding <AJMCoding@users.noreply.github.com>
2024-11-29 15:42:53 +0000

    deploy: 25021ffe0a631a34e5c1a2cb94b0f33e891699bc

--- namespaceconfigfile.js
+++ namespaceconfigfile.js
@@ -7,19 +7,19 @@ var namespaceconfigfile =
     [ "t_Config_data", "namespaceconfigfile.html#a520a94aa797db764619253725c74b2d8", null ],
     [ "t_location", "namespaceconfigfile.html#a518063eadf2a848d0f652b0f79bc3128", null ],
     [ "t_server", "namespaceconfigfile.html#aa80825aa8468926c8586f8d6f17a9643", null ],
-    [ "cmd_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5", [
-      [ "server_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5a3c6f2fa601dbc6f7c1ee681f1cc4d196", null ],
-      [ "listen_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5a88ee3cfbc5e92fba6cd12991c02a8cf5", null ],
-      [ "server_name_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5a66e2df8d94b887df443dc66f2f878a6a", null ],
-      [ "error_page_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5a59327affec036046c2578caef2f4ad72", null ],
-      [ "client_max_body_size_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5ac09d3eefc886257fee637b637eb8085f", null ],
-      [ "location_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5aa83957b2a8e5f31891970cf6ab25cfca", null ],
-      [ "limit_except_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5a65e0dacb233c25d26a9e502d623c169f", null ],
-      [ "return_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5afc9d122b2fa97ced954ffcb15fee3169", null ],
-      [ "root_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5a3f40f00bf02453c5a83cd9f8ca4dcd1e", null ],
-      [ "autoindex_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5a331cbc0c90897766c5b4e49c1b1979af", null ],
-      [ "index_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5a3ccdcf4baf5d024c386f692efc301066", null ],
-      [ "unknown_id", "namespaceconfigfile.html#a1d045be0bbeb686c82c3e422bcb74bf5ac116da056a08f1ca55a2a39595d41357", null ]
+    [ "CmdId", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1", [
+      [ "SERVER_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1a6d42f748e039b95b68d94268650b9a1a", null ],
+      [ "LISTEN_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1a85b148d663bcbd721ed19a8b0b90c329", null ],
+      [ "SERVER_NAME_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1afb14ab889353e278bb2fefe66d37c5c5", null ],
+      [ "ERROR_PAGE_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1a04c3a965202b115247c44e21216c5c03", null ],
+      [ "CLIENT_MAX_BODY_SIZE_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1ad8822d02c377131b7a8025c61e0cbf32", null ],
+      [ "LOCATION_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1ae15666574760c3ec8694c1d7ae70bffe", null ],
+      [ "LIMIT_EXCEPT_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1a14d42be51c247915b4f2e589ded07141", null ],
+      [ "RETURN_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1aa37d58dc2012e7e62fbfc055c143d887", null ],
+      [ "ROOT_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1a0c9a7787a881f83be310823464cd4267", null ],
+      [ "AUTOINDEX_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1a8b058e1cb2e2bec7ddabf30d3ee81d70", null ],
+      [ "INDEX_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1a0b280782cfe14024ae0a0aa56a92d591", null ],
+      [ "UNKNOWN_ID", "namespaceconfigfile.html#a31714c3083685cd75a4fb5991a6e8cf1a16366376c41ea41ef41179179e7dda70", null ]
     ] ],
     [ "ft_stoi", "namespaceconfigfile.html#ad34d46b490514a9d5c9408620ed5e006", null ],
     [ "is_number", "namespaceconfigfile.html#abab751401cc4565602552b0da2b831aa", null ]
