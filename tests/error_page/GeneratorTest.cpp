ce4eb52ba34a4e09863238cec7e3d4f9d621658d
AJMCoding <felix.stark.26.7@gmail.comexport>
2024-12-02 14:50:19 +0000

    feat: automatic Error page generation based on error code.

--- tests/error_page/GeneratorTest.cpp
+++ tests/error_page/GeneratorTest.cpp
@@ -5,12 +5,31 @@
 namespace error_page {
 
 class GeneratorTest : public ::testing::Test {
-protected:
-    Generator instance; // Instance of the class being tested
+	
 };
 
-TEST_F(GeneratorTest, DefaultConstructor) {
-    ASSERT_NO_THROW(Generator new_instance;);
+TEST_F(GeneratorTest, CorrectUsage) {
+	
+	std::string error_page = generateErrorPage(404);
+	ASSERT_NE(error_page, "");
+}
+
+TEST_F(GeneratorTest, IncorrectNumber) {
+	
+	std::string error_page = generateErrorPage(600);
+	ASSERT_EQ(error_page, "");
+}
+
+TEST_F(GeneratorTest, NegativeNumber) {
+	
+	std::string error_page = generateErrorPage(-1);
+	ASSERT_EQ(error_page, "");
+}
+
+TEST_F(GeneratorTest, Zero) {
+	
+	std::string error_page = generateErrorPage(0);
+	ASSERT_EQ(error_page, "");
 }
 
 } /* namespace error_page */
