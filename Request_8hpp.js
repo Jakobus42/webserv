c6ede2553bbc4c55d1475cd93f1f8d215057eca1
rwxg <rwxg@users.noreply.github.com>
2024-12-05 17:51:04 +0000

    deploy: ae3f48d8eec61be2184a7cd4794f19083d9959a0

--- Request_8hpp.js
+++ Request_8hpp.js
@@ -1,5 +1,16 @@
 var Request_8hpp =
 [
+    [ "s_requestData", "structhttp_1_1s__requestData.html", "structhttp_1_1s__requestData" ],
+    [ "parsingData", "structhttp_1_1parsingData.html", "structhttp_1_1parsingData" ],
     [ "Request", "classhttp_1_1Request.html", "classhttp_1_1Request" ],
-    [ "BUFFER_SIZE", "Request_8hpp.html#a6b20d41d6252e9871430c242cb1a56e7", null ]
+    [ "BUFFER_SIZE", "Request_8hpp.html#a6b20d41d6252e9871430c242cb1a56e7", null ],
+    [ "t_parsingData", "Request_8hpp.html#acefbcae490658d0f2960c373a9535648", null ],
+    [ "t_requestData", "Request_8hpp.html#a344224153c4286273f922e54b792a50d", null ],
+    [ "RequestStatus", "Request_8hpp.html#a068cab601142fb739077ccd62379df64", [
+      [ "PARSE_START", "Request_8hpp.html#a068cab601142fb739077ccd62379df64af1ec52e66d8011ff2329d6937794bdb3", null ],
+      [ "PARSE_HEAD", "Request_8hpp.html#a068cab601142fb739077ccd62379df64a6e55d4fbcb4414ad97de58c56bfc3654", null ],
+      [ "PARSE_HEADERS", "Request_8hpp.html#a068cab601142fb739077ccd62379df64a7bb2006ced5537e4b6ab2f88b3246ebc", null ],
+      [ "PARSE_BODY", "Request_8hpp.html#a068cab601142fb739077ccd62379df64ac4ffb0e12ded445e68ca3f683a01ba21", null ],
+      [ "PARSE_END", "Request_8hpp.html#a068cab601142fb739077ccd62379df64a5becdb8b02d81768bd29e3315117c7ff", null ]
+    ] ]
 ];
