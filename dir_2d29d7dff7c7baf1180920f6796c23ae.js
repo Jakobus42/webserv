5a4d4c56b649d1ac047bd07c7ace02d858311372
Jakobus42 <Jakobus42@users.noreply.github.com>
2024-12-05 18:08:08 +0000

    deploy: 77ac54d55a221ed8e5abf20f0b9e3cd617bb05e5

--- dir_2d29d7dff7c7baf1180920f6796c23ae.js
+++ dir_2d29d7dff7c7baf1180920f6796c23ae.js
@@ -3,9 +3,6 @@ var dir_2d29d7dff7c7baf1180920f6796c23ae =
     [ "ClientSocket.cpp", "ClientSocket_8cpp.html", null ],
     [ "Connection.cpp", "Connection_8cpp.html", null ],
     [ "errorPageGenerator.cpp", "errorPageGenerator_8cpp.html", "errorPageGenerator_8cpp" ],
-    [ "Parse_body.cpp", "Parse__body_8cpp.html", null ],
-    [ "Parse_head.cpp", "Parse__head_8cpp.html", null ],
-    [ "Parse_headers.cpp", "Parse__headers_8cpp.html", null ],
     [ "Request.cpp", "Request_8cpp.html", null ],
     [ "ServerSocket.cpp", "ServerSocket_8cpp.html", "ServerSocket_8cpp" ],
     [ "VirtualServer.cpp", "VirtualServer_8cpp.html", null ]
--- structhttp_1_1parsingData.js
+++
@@ -1,5 +0,0 @@
-var structhttp_1_1parsingData =
-[
-    [ "pos", "structhttp_1_1parsingData.html#a2f63e57aae90958ca56842d2a533efac", null ],
-    [ "rest", "structhttp_1_1parsingData.html#a23f8b5721d6de6a4af15ba2294cd37bf", null ]
-];
