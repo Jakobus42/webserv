361f6e27d1d1f71caaf2f2ad783ddc7283a8b554
AJMCoding <AJMCoding@users.noreply.github.com>
2024-12-19 14:05:03 +0000

    deploy: d870a433602b7aaa6cad068c815aaaacc06b997b

--- namespaceshared.js
+++ namespaceshared.js
@@ -4,7 +4,7 @@ var namespaceshared =
       [ "isGreyspace", "namespaceshared_1_1string.html#a2c0ea281fde1150615baa77e6dcce5d3", null ],
       [ "posStoi", "namespaceshared_1_1string.html#a66dbf48761b7b07dd8b7001a6e1d6c56", null ],
       [ "split", "namespaceshared_1_1string.html#aa68d026861d0f4c81179e94efca2ed7c", null ],
-      [ "StoiHex", "namespaceshared_1_1string.html#a11b92e79ee1735a5a934a9c2981f3709", null ],
+      [ "StoiHex", "namespaceshared_1_1string.html#a0f7b586737ab36eade3fc402c9abe0a7", null ],
       [ "trim", "namespaceshared_1_1string.html#a548d2887496a48cb38f3ab46dd407c6b", null ],
       [ "WHITESPACES", "namespaceshared_1_1string.html#a5841ad66349ecb863209150dfd2b0192", null ]
     ] ]
--- stringUtils_8hpp.js
+++ stringUtils_8hpp.js
@@ -3,7 +3,7 @@ var stringUtils_8hpp =
     [ "isGreyspace", "stringUtils_8hpp.html#a2c0ea281fde1150615baa77e6dcce5d3", null ],
     [ "posStoi", "stringUtils_8hpp.html#a66dbf48761b7b07dd8b7001a6e1d6c56", null ],
     [ "split", "stringUtils_8hpp.html#aa68d026861d0f4c81179e94efca2ed7c", null ],
-    [ "StoiHex", "stringUtils_8hpp.html#a11b92e79ee1735a5a934a9c2981f3709", null ],
+    [ "StoiHex", "stringUtils_8hpp.html#a0f7b586737ab36eade3fc402c9abe0a7", null ],
     [ "trim", "stringUtils_8hpp.html#a548d2887496a48cb38f3ab46dd407c6b", null ],
     [ "WHITESPACES", "stringUtils_8hpp.html#a5841ad66349ecb863209150dfd2b0192", null ]
 ];
