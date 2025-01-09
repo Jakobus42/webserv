3401b185b422aa0384926f68ccd1ec607cc2d95a
Jakobus42 <Jakob.sadjina@icloud.com>
2025-01-09 21:02:02 +0000

    refactor: combined parser files into one

--- src/http/RequestParser.cpp
+++ src/http/RequestParser.cpp
@@ -128,4 +128,130 @@ namespace http {
 		return true;
 	}
 
+	bool Request::checkHead(const std::vector<std::string>& args) {
+		if (args[0] != "GET" && args[0] != "POST" && args[0] != "DELETE") {
+			return false;
+		}
+		if (args[2] != "HTTP/1.1") {
+			return false;
+		}
+		return true;
+	}
+
+	bool Request::parseHead(std::string& input) {
+		std::string line;
+		GetLineStatus status = getNextLineHTTP(input, line);
+		if (status == GET_LINE_ERROR)
+			return false;
+		if (status == GET_LINE_END)
+			return true;
+		if (line == "") {
+			LOG("Request: Error: Empty line in request line", 1);
+			return false;
+		}
+		std::string key;
+		std::vector<std::string> args;
+		unsigned long pos = 0;
+		for (int a = 0; a < 3; a++) {
+			while (pos < line.size() && line[pos] != ' ') {
+				key += line[pos];
+				pos++;
+			}
+			pos++;
+			args.push_back(key);
+			key.clear();
+		}
+		if (args.size() == 3 && checkHead(args) == true) {
+			m_requestData.method = args[0];
+			m_requestData.uri = args[1];
+			m_requestData.version = args[2];
+			m_status = PARSE_HEADERS;
+			return true;
+		} else {
+			LOG("Request: Error: Invalid request line", 1);
+			return false;
+		}
+	}
+
+	bool Request::parseBody(std::string& input) {
+		if (m_expectedBody == NO_BODY) {
+			m_status = PARSE_END;
+			return true; // error if body exists?
+		}
+		if (m_expectedBody == CHUNKED) {
+			return parseBodyChunked(input);
+		}
+		if (m_expectedBody == CONTENT_LENGTH) {
+			if (m_requestData.body.size() + input.size() < m_contentLength) {
+				m_requestData.body += input;
+				input = "";
+			} else {
+				m_requestData.body += input.substr(0, m_contentLength - m_requestData.body.size());
+				input = input.substr(m_contentLength - m_requestData.body.size());
+				m_status = PARSE_END;
+			}
+		}
+		return true;
+	}
+
+	bool Request::parseBodyChunked(std::string& input) {
+		while (1) {
+			if (m_chunkedStatus == CHUNK_SIZE) {
+				std::string line;
+				GetLineStatus status = getNextLineHTTP(input, line);
+				if (status == GET_LINE_ERROR) {
+					return false;
+				}
+				if (status == GET_LINE_OK) {
+					if (line == "") {
+						LOG("Error: Empty line in chunked encoding", 1);
+						return false;
+					} else {
+						int ret = 0;
+						m_contentLength = shared::string::StoiHex(line, ret);
+						if (ret == -1) {
+							LOG("Error: Invalid hexadecimal number in chunked encoding", 1);
+							return false;
+						}
+						if (m_contentLength == 0) {
+							m_chunkedStatus = CHUNK_END;
+						} else {
+							m_chunkedStatus = CHUNK_DATA;
+						}
+						// if (line != "") { //TODO: chunk extension
+					}
+				}
+				if (m_chunkedStatus == CHUNK_SIZE) {
+					return true;
+				}
+			}
+			if (m_chunkedStatus == CHUNK_DATA) {
+				std::string chunk = input.substr(0, m_contentLength);
+				m_contentLength -= chunk.size();
+				m_requestData.body += chunk;
+				input = input.substr(chunk.size());
+				if (m_contentLength == 0) {
+					m_chunkedStatus = CHUNK_DATA_END;
+				} else {
+					return true;
+				}
+			}
+			if (m_chunkedStatus == CHUNK_DATA_END) {
+				if (input.length() < 2) {
+					return true;
+				}
+				if (input[0] == '\r' && input[1] == '\n') {
+					input = input.substr(2);
+					m_chunkedStatus = CHUNK_SIZE;
+				} else {
+					LOG("Error: Invalid chunked encoding, \r\n expected after chunk.", 1);
+					return false;
+				}
+			}
+			if (m_chunkedStatus == CHUNK_END) {
+				return parseHeaders(input, TRAILING);
+			}
+		}
+	}
+
 } /* namespace http */
