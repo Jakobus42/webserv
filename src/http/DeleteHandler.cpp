6183cdfb02393699463e0f0acef84a841fb8b6dc
rwxg <xaver.goessinger@gmail.com>
2025-01-30 19:24:40 +0000

    feat(requestHandlers): create basic requestHandler

--- src/http/DeleteHandler.cpp
+++ src/http/DeleteHandler.cpp
@@ -1,5 +1,7 @@
 #include "http/DeleteHandler.hpp"
 
+#include <cstdio>
+
 namespace http {
 
 	/**
@@ -16,8 +18,16 @@ namespace http {
 	}
 
 	void DeleteHandler::handle(const Request& request, Response& response) {
-		(void)request;
-		(void)response;
+		if (false) {								   // if (fileCantBeFound() || fileCantBeDeleted())
+			response.setBody(getErrorPage(NOT_FOUND)); // or FORBIDDEN, or maybe something else...
+			return handleError(request, response);
+		} else {
+			std::remove(request.getUri().path.c_str()); // unlink the file
+			// either return 204 No Content when there is no body
+			// or return 200 OK if there's information in the body
+			// i.e. a html page saying "file /foo/bar.baz was deleted"
+			response.setStatusCode(OK);
+		}
 	}
 
 } /* namespace http */
