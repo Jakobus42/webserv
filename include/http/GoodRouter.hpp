c9a9bc1e8001d5c8241baaae90cbff9b5931f472
rwxg <xaver.goessinger@gmail.com>
2025-02-04 17:55:05 +0000

    feat(router): create new GoodRouter class

---
+++ include/http/GoodRouter.hpp
@@ -0,0 +1,125 @@
+#pragma once
+
+#include "config/Parser.hpp"
+#include "http/types.hpp"
+
+#include <string>
+#include <vector>
+
+namespace http {
+	class GoodRouter {
+		public:
+			GoodRouter(const std::vector<config::Location>& locations, const config::Location& globalRoot);
+			~GoodRouter();
+			GoodRouter(const GoodRouter& other);
+			const GoodRouter& operator=(const GoodRouter& rhs);
+
+		public:
+			/**
+			 * @brief Checks whether a path exists in the file system, and whether it is a file or directory
+			 *
+			 * @param absolutePath The absolute path to check
+			 * @return FileType to indicate the type: FILE, DIRECTORY or _NOT_FOUND if the path doesn't exist
+			 */
+			static FileType checkFileType(const std::string& absolutePath);
+
+			/**
+			 * @brief Splits a path into its components, delimited by '/'
+			 *
+			 * @param path The path to split
+			 * @return std::vector<std::string> The individual path components
+			 * @throws http::exception if the path is invalid
+			 */
+			static std::vector<std::string> splitPath(const std::string& path) throw(http::exception);
+
+			/**
+			 * @brief Joins a split path into a string, delimited by '/'
+			 *        Should never receive broken data
+			 *
+			 * @param pathComponents The components to join
+			 * @return std::string The composed path
+			 */
+			static std::string joinPath(const std::vector<std::string>& pathComponents);
+
+			/**
+			 * @brief Normalizes a path against a basePath (globalRoot by default)
+			 *        Paths can be relative (use ./ and ../), but may not
+			 *        go lower than the basePath.
+			 *
+			 * @param path The path to normalize split into components by splitPath()
+			 * @param basePath The root path which the path may not escape, split by splitPath()
+			 * @return std::vector<std::string> The normalized path, without any "./" or "../"
+			 */
+			static std::vector<std::string> normalizePath(const std::vector<std::string>& path);
+
+			/**
+			 * @brief Generate the absolute path for a location and an optional subPath
+			 *
+			 * @param location The location of which to retrieve the root path
+			 * @param subPath The subdirectory to retrieve in that root path
+			 * @return std::string The absolute path generated from  <location.path>/<subPath>
+			 */
+			std::string findAbsolutePath(const config::Location& location, const std::string& subPath = "");
+
+			/**
+			 * @brief Traverses m_locations according to a Request's Uri,
+			 *        respecting redirections while avoiding redirect loops
+			 *
+			 * @param uri The URI requested by the client
+			 * @return std::string The absolute, normalized path to a file or directory
+			 * @throws http::exception upon any issues encountered during routing
+			 */
+			std::string routeToPath(const std::vector<std::string>& uriPath, const config::Location& currentLocation, const std::vector<std::string>& currentRootPath, std::size_t redirects = 0, std::size_t depth = 0) throw(http::exception);
+
+			/**
+			 * Router needs to:
+			 *
+			 * - Normalize paths versus a certain base path
+			 *   -> normalizePath();
+			 *
+			 * - Split a path into its components
+			 *   and (?) validate whether it is even a valid path
+			 *   -> splitPath();
+			 *
+			 * - Construct and return the absolute path from a Location,
+			 *   the rest of the uri that wasn't consumed while routing,
+			 *   and the global base path that should be generated on startup
+			 *   -> generateAbsolutePath();
+			 *
+			 * - Identify whether an absolute path exists in the file system,
+			 *   whether it is a file or a directory, and whether
+			 *   that file or directory is accessible
+			 *
+			 * - Traverse redirects as part of the routing process,
+			 *   detect loops caused by circular redirects and
+			 *   limit total redirects
+			 *
+			 *
+			 * This may be achieved by storing all components of the uri path
+			 * in something like a queue of strings, then looking for a match
+			 * of the first component in locations. If found,
+			 * remove that component from the queue, set the currentLocation
+			 * to that location's root, and look for the next component in
+			 * that location's children.
+			 *
+			 * Once a location doesn't have a child matching the next component,
+			 * or when the last component is reached, store
+			 * any remaining components as the subdirectory. Then, look
+			 * for that subdirectory (and / or file) in the current Location's
+			 * root path. All Location's root paths are relative to the globalRoot.
+			 * If a Location doesn't have a root path (or the root path is empty),
+			 * just use the globalRoot Location instead.
+			 *
+			 * TODO: Parser -> The globalRoot should already contain the dataDir.
+			 * e.g.: data_dir = /www and global_root = /var/server in config,
+			 * the globalRoot Location should have the path /var/server/www.
+			 *
+			 */
+
+		private:
+			static const std::size_t MAX_REDIRECTS = 32;
+
+			std::vector<config::Location> m_locations;
+			config::Location m_globalRoot;
+	};
+} // namespace http
---
+++ src/http/GoodRouter.cpp
@@ -0,0 +1,141 @@
+#include "http/GoodRouter.hpp"
+
+#include <sys/stat.h>
+#include <unistd.h>
+
+namespace http {
+
+	/**
+	 * @brief Constructs a new GoodRouter object.
+	 */
+	GoodRouter::GoodRouter(const std::vector<config::Location>& locations, const config::Location& globalRoot)
+		: m_locations(locations)
+		, m_globalRoot(globalRoot) {}
+
+	/**
+	 * @brief Destroys the GoodRouter object.
+	 */
+	GoodRouter::~GoodRouter() {}
+
+	GoodRouter::GoodRouter(const GoodRouter& other)
+		: m_locations(other.m_locations)
+		, m_globalRoot(other.m_globalRoot) {}
+
+	const GoodRouter& GoodRouter::operator=(const GoodRouter& rhs) {
+		if (this == &rhs) {
+			return *this;
+		}
+		m_locations = rhs.m_locations;
+		m_globalRoot = rhs.m_globalRoot;
+		return *this;
+	}
+
+	//
+
+	FileType checkFileType(const std::string& absolutePath) {
+		struct stat statBuf;
+		if (stat(absolutePath.c_str(), &statBuf) != 0) {
+			return _NOT_FOUND; // Path does not exist
+		}
+		if (S_ISDIR(statBuf.st_mode)) {
+			return DIRECTORY;
+		} else if (S_ISREG(statBuf.st_mode)) {
+			return FILE;
+		}
+		return _NOT_FOUND; // Other types (not file/dir)
+	}
+
+	std::vector<std::string> GoodRouter::splitPath(const std::string& path) throw(http::exception) {
+		std::vector<std::string> tokens;
+		std::cout << "Splitting path: " << path << std::endl;
+		if (path.empty()) {
+			throw http::exception(NOT_FOUND, "Path is empty");
+		}
+		if (path[0] != '/') {
+			throw http::exception(BAD_REQUEST, "Path doesn't begin with '/'");
+		}
+
+		std::stringstream ss(path.substr(1)); // Remove leading '/'
+		std::string segment;
+		while (std::getline(ss, segment, '/')) {
+			if (!segment.empty()) {
+				tokens.push_back(segment);
+			}
+		}
+		return tokens;
+	}
+
+	std::string GoodRouter::joinPath(const std::vector<std::string>& pathComponents) {
+		std::string joined = "/";
+		for (std::vector<std::string>::const_iterator it = pathComponents.begin(); it != pathComponents.end(); ++it) {
+			joined += *it;
+			joined += "/";
+		}
+		return joined;
+	}
+
+	std::vector<std::string> GoodRouter::normalizePath(const std::vector<std::string>& path) {
+		std::vector<std::string> normalized;
+
+		for (std::vector<std::string>::const_iterator it = path.begin(); it != path.end(); ++it) {
+			if (*it == "..") {
+				if (!normalized.empty()) {
+					normalized.pop_back();
+				}
+			} else if (!it->empty() && *it != ".") {
+				normalized.push_back(*it);
+			}
+		}
+		return normalized;
+	}
+
+	std::string GoodRouter::findAbsolutePath(const config::Location& location, const std::string& subPath) {
+		std::string absolutePath = joinPath(m_globalRoot.root) + joinPath(location.root);
+		return absolutePath + subPath;
+	}
+
+	// sample GET /foo/bar/baz
+	/************************
+	 * root /var/server;
+	 * location /foo {
+	 * 	return /qux/quux;
+	 * }
+	 * location /qux {
+	 * 	location /quux {
+	 * 		root /quux;
+	 * 	}
+	 * }
+	 ************************/
+	std::string GoodRouter::routeToPath(
+		// const Uri& uri, // TODO: don't need the whole uri, pathSegments is enough
+		const std::vector<std::string>& uriPath,		 // the requests' path that we're traversing
+		const config::Location& currentLocation,		 // Location we're currently inside of, default should be m_globalRoot
+		const std::vector<std::string>& currentRootPath, // nearest 'root', default should be m_globalRoot.path
+		std::size_t redirects,
+		std::size_t depth) throw(http::exception) {
+		if (depth > MAX_REDIRECTS) {
+			throw http::exception(LOOP_DETECTED, "Depth exceeded MAX_REDIRECTS");
+		}
+		if (redirects > MAX_REDIRECTS) {
+			throw http::exception(LOOP_DETECTED, "Redirects exceeded MAX_REDIRECTS");
+		}
+		if (currentLocation.hasRedirect()) {
+			return routeToPath(currentLocation.redirectUriTokens, m_globalRoot, m_globalRoot.path, redirects + 1); // TODO: invalid, this would then always return globalRoot's route
+		}																										   // TODO: how the frick do we solve this?; TODO: doesn't currently count redirects
+		if (uriPath.size() <= depth) {
+			return joinPath(currentRootPath); // TODO: I think this doesn't set the root path properly yet, does it?
+		}
+		for (std::vector<config::Location>::const_iterator loc = currentLocation.locations.begin(); loc != currentLocation.locations.end(); ++loc) {
+			if (loc->path[0] == uriPath.at(depth)) {
+				const std::vector<std::string>& nextRootPath = !loc->root.empty() ? loc->root : currentRootPath; // TODO: assign inherited rootPath during parsing
+				return routeToPath(uriPath, *loc, nextRootPath, redirects, depth + 1);							 // use nearest parent
+			}
+		}
+		std::vector<std::string> subDirectory(uriPath.begin() + depth, uriPath.end()); // TODO: scary pointer math, ensure this never breaks
+		if (currentLocation.root.empty()) {
+			return joinPath(currentRootPath) + joinPath(subDirectory);
+		} else {
+			return joinPath(currentRootPath) + joinPath(currentLocation.root) + joinPath(subDirectory);
+		}
+	}
+} // namespace http
