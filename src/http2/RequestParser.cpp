b243451bab072025420ca72d592737d9ed3bad6c
Jakobus42 <jakob.sadjina@icloud.com>
2025-03-04 22:39:30 +0000

    feat: Abstract message parser

---
+++ src/http2/RequestParser.cpp
@@ -0,0 +1,17 @@
+#include "http2/RequestParser.hpp"
+
+namespace http2 {
+
+	/**
+	 * @brief Constructs a new RequestParser object.
+	 */
+	RequestParser::RequestParser() {
+	}
+
+	/**
+	 * @brief Destroys the RequestParser object.
+	 */
+	RequestParser::~RequestParser() {
+	}
+
+} /* namespace http2 */
