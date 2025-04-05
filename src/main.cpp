fad1a760b5eacba01e506a191d9f653642b792f2
Jakobus42 <jakob.sadjina@icloud.com>
2025-04-05 18:04:43 +0200

    refactor: parser parser parser

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@ int main() {
 	if (!configParser.parseFile("config/config.conf")) {
 		return 1;
 	}
-	// std::vector<config::Server>& serverConfig = configParser.getConfigs();
-	// for (std::size_t i = 0; i < serverConfig.size(); ++i) {
-	// 	serverConfig[i].print();
-	// }
+	std::vector<config::Server>& serverConfig = configParser.getConfigs();
+	for (std::size_t i = 0; i < serverConfig.size(); ++i) {
+		serverConfig[i].print();
+	}
 	return 0;
 }
