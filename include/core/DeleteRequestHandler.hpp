cef970bdb79bc14ae19786ec6cdab2f1f24b42cb
rwxg <xaver.goessinger@gmail.com>
2025-05-01 12:08:22 +0200

    feat(requestHandlers): implement DELETE handler

--- include/core/DeleteRequestHandler.hpp
+++ include/core/DeleteRequestHandler.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "core/ARequestHandler.hpp"
+#include "http/http.hpp"
+#include "shared/fileUtils.hpp"
 
 namespace core {
 
@@ -9,7 +11,10 @@ namespace core {
 			DeleteRequestHandler();
 			virtual ~DeleteRequestHandler();
 
-			virtual bool handle(const http::Request& request, http::Response& response);
+			virtual bool handle(const http::Request& request, http::Response& response) throw(http::HttpException);
+
+			void checkFileAccess() const throw(http::HttpException);
+			void deleteFile() throw(http::HttpException);
 	};
 
 } /* namespace core */
--- src/core/DeleteRequestHandler.cpp
+++ src/core/DeleteRequestHandler.cpp
@@ -1,15 +1,49 @@
 #include "core/DeleteRequestHandler.hpp"
 
+#include "http/Response.hpp"
+
+#include <cstdio>
+
 namespace core {
 
 	DeleteRequestHandler::DeleteRequestHandler()
-		: ARequestHandler() {
+		: ARequestHandler() {}
+
+	DeleteRequestHandler::~DeleteRequestHandler() {}
+
+	// todo: ensure these codes are correct
+	// todo: maybe check for F_OK and W_OK, maybe more?
+	void DeleteRequestHandler::checkFileAccess() const throw(http::HttpException) {
+		shared::file::FileType fileType = shared::file::getFileType(m_absoluteFilePath);
+
+		if (fileType == shared::file::NOT_FOUND) {
+			throw http::HttpException(http::NOT_FOUND, "DELETE: File doesn't exist");
+		}
+		if (fileType == shared::file::DIRECTORY) {
+			throw http::HttpException(http::FORBIDDEN, "DELETE: Expected file; received directory");
+		}
 	}
 
-	DeleteRequestHandler::~DeleteRequestHandler() {
+	void DeleteRequestHandler::deleteFile() throw(http::HttpException) {
+		if (std::remove(m_absoluteFilePath.c_str()) != 0) {
+			throw http::HttpException(http::FORBIDDEN, "DELETE: File could not be removed");
+		}
 	}
 
-	bool DeleteRequestHandler::handle(const http::Request&, http::Response&) {
+	bool DeleteRequestHandler::handle(const http::Request&, http::Response& response) throw(http::HttpException) {
+		switch (m_state) {
+			case PREPROCESS:
+				checkFileAccess();
+				m_state = PROCESS;
+			case PROCESS: {
+				deleteFile();
+				response.setStatusCode(http::NO_CONTENT);
+				m_state = DONE;
+				return false;
+			}
+			case DONE:
+				return false;
+		}
 		return false;
 	}
 
