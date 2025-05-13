f33cc798c82a426166e17c9d3cfad2a56932427e
Jakobus42 <jakob.sadjina@icloud.com>
2025-05-13 16:48:08 +0200

    style: rename use deafult for more clarity

--- src/core/RequestProcessor.cpp
+++ src/core/RequestProcessor.cpp
@@ -37,22 +37,22 @@ namespace core {
 		m_handlers[http::DELETE] = new DeleteRequestHandler();
 	}
 
-	//todo: this could be done in the router if we edit the config correctly that the cgi only uses global configs
+	// todo: this could be done in the router if we edit the config correctly that the cgi only uses global configs
 	void RequestProcessor::resolveHost(const http::Request& request) {
 		const std::string& host = request.getUri().getAuthority();
-		bool useDefault = true;
+		bool useDefaultConfig = true;
 
 		for (std::size_t i = 0; i < m_serverConfigs.size(); ++i) {
 			config::ServerConfig serverConfig = m_serverConfigs[i];
 			for (std::size_t j = 0; j < serverConfig.serverNames.size(); ++j) {
 				if (serverConfig.serverNames[j] == host) {
 					m_serverConfig = serverConfig;
-					useDefault = false;
+					useDefaultConfig = false;
 					break;
 				}
 			}
 		}
-		if (useDefault) {
+		if (useDefaultConfig) {
 			m_serverConfig = m_serverConfigs.at(0);
 		}
 	}
