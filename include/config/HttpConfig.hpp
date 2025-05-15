d763161e2272f4c219c81c53dd12766f5ceb8b9f
rwxg <xaver.goessinger@gmail.com>
2025-05-15 16:29:52 +0200

    feat(config): create HttpConfig class

---
+++ include/config/HttpConfig.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <map>
+#include <string>
+
+namespace config {
+
+	struct HttpConfig {
+		public:
+			unsigned long maxBodySize;
+			unsigned long maxHeaderValueLength;
+			unsigned long maxHeaderCount;
+			unsigned long maxHeaderValueCount;
+			unsigned long maxHeaderNameLength;
+			unsigned long connectionTimeout;
+			unsigned long cgiTimeout;
+			unsigned long maxUriLength;
+			std::string dataDirectory;
+			std::map<std::string, std::string> cgiInterpreters;
+
+			HttpConfig();
+			~HttpConfig();
+			HttpConfig(const HttpConfig& other);
+			HttpConfig& operator=(const HttpConfig& rhs);
+
+			void print() const;
+			void validate() const;
+	};
+
+} /* namespace config */
---
+++ src/config/HttpConfig.cpp
@@ -0,0 +1,77 @@
+#include "config/HttpConfig.hpp"
+
+#include "config/Parser.hpp" // todo: PLEASE MOVE CONFIG AND EXCEPTION TO SEPARATE FILES
+
+#include <iostream>
+
+namespace config {
+
+	HttpConfig::HttpConfig()
+		: maxBodySize(10 * 1024 * 1024)	 // 10MB
+		, maxHeaderValueLength(8 * 1024) // 8KB
+		, maxHeaderCount(128)			 // 128 headers
+		, maxHeaderValueCount(64)		 // 64 values
+		, maxHeaderNameLength(256)		 // 256B
+		, connectionTimeout(60)
+		, cgiTimeout(60)
+		, maxUriLength(1024)
+		, dataDirectory("")
+		, cgiInterpreters() {}
+
+	HttpConfig::~HttpConfig() {}
+
+	HttpConfig::HttpConfig(const HttpConfig& other)
+		: maxBodySize(other.maxBodySize)
+		, maxHeaderValueLength(other.maxHeaderValueLength)
+		, maxHeaderCount(other.maxHeaderCount)
+		, maxHeaderValueCount(other.maxHeaderValueCount)
+		, maxHeaderNameLength(other.maxHeaderNameLength)
+		, connectionTimeout(other.connectionTimeout)
+		, cgiTimeout(other.cgiTimeout)
+		, maxUriLength(other.maxUriLength)
+		, dataDirectory(other.dataDirectory)
+		, cgiInterpreters(other.cgiInterpreters) {}
+
+	HttpConfig& HttpConfig::operator=(const HttpConfig& rhs) {
+		if (this == &rhs) {
+			return *this;
+		}
+		maxBodySize = rhs.maxBodySize;
+		maxHeaderCount = rhs.maxHeaderCount;
+		maxHeaderNameLength = rhs.maxHeaderNameLength;
+		maxHeaderValueCount = rhs.maxHeaderValueCount;
+		maxHeaderValueLength = rhs.maxHeaderValueLength;
+		connectionTimeout = rhs.connectionTimeout;
+		cgiTimeout = rhs.cgiTimeout;
+		maxUriLength = rhs.maxUriLength;
+		dataDirectory = rhs.dataDirectory;
+		cgiInterpreters = rhs.cgiInterpreters;
+		return *this;
+	}
+
+	void HttpConfig::print() const {
+		std::cout << "-------- Http Config: --------" << std::endl;
+
+		std::cout << "MaxBodySize: " << maxBodySize << std::endl;
+		std::cout << "MaxHeaderCount: " << maxHeaderCount << std::endl;
+		std::cout << "MaxHeaderNameLength: " << maxHeaderNameLength << std::endl;
+		std::cout << "MaxHeaderValueCount: " << maxHeaderValueCount << std::endl;
+		std::cout << "MaxHeadeaValueLength: " << maxHeaderValueLength << std::endl;
+		std::cout << "ConnectionTimeout: " << connectionTimeout << std::endl;
+		std::cout << "cgiTimeout: " << cgiTimeout << std::endl;
+		std::cout << "maxUriLength: " << maxUriLength << std::endl;
+		std::cout << "dataDirectory: " << dataDirectory << std::endl;
+		for (std::map<std::string, std::string>::const_iterator interpreter = cgiInterpreters.begin(); interpreter != cgiInterpreters.end(); ++interpreter) {
+			std::cout << interpreter->first << " => " << interpreter->second << std::endl;
+		}
+
+		std::cout << "--------------------------------" << std::endl;
+	}
+
+	void HttpConfig::validate() const {
+		if (dataDirectory.empty()) {
+			throw parse_exception("HTTP block requires a 'data_dir'");
+		}
+	}
+
+} /* namespace config */
